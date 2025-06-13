daf298c2260d235ea264ced6665158292b497183
Krzysztof Waszkowski <pleaseopenissue@example.com>
2025-06-13 13:55:27 +0200

    Added database entries

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('courses/', views.course_table, name='course_table'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
-# Create your views here.
 from django.shortcuts import render
+from .models import Course  # import the model
 
 def home(request):
-    return render(request, "core/home.html")
+    courses = Course.objects.all().order_by("date")
+    return render(request, "core/home.html", {"courses": courses})
+
+def course_table(request):
+    courses = Course.objects.all().order_by("date")
+    return render(request, "core/courses.html", {"courses": courses})
