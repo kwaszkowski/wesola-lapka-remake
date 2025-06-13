124005da769238023f3bc1bf7c3b9469472775bc
Krzysztof Waszkowski <pleaseopenissue@example.com>
2025-06-13 13:39:11 +0200

    Finished about

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)          # Nazwa kursu
+    scope = models.CharField(max_length=200)         # Zakres
+    location = models.CharField(max_length=100)      # Miejscowość
+    date = models.DateField()                        # Data
+
+    def __str__(self):
+        return self.name
